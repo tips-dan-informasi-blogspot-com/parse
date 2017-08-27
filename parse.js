d1c6d5d042e9859114e3d5a370b894fa4a57a264
Tips Berbagi Info <30714513+newbiejavascript@users.noreply.github.com>
2017-08-27 23:19:19 +0700

    Update parse.js

--- parse.js
+++ parse.js
@@ -1,13 +1,13 @@
-//<![CDATA[ //hakcipta: www.tips-berbagiifo.blogspot.com
+//<![CDATA[
 function tipsdaninformasi(){
-var re=/[<>]/g
-for (i=0; i<arguments.length; i++) //hakcipta: www.tips-berbagiifo.blogspot.com
-arguments[i].value=arguments[i].value.replace(re, function(m){return replacechar(m)})
-} //hakcipta: www.tips-berbagiifo.blogspot.com
+var textareas=/[<>]/g
+for (i=0; i<arguments.length; i++)
+arguments[i].value=arguments[i].value.replace(textareas, function(m){return replacechar(m)})
+}
 function replacechar(match){
-if (match=="<") //hakcipta: www.tips-berbagiifo.blogspot.com
-return "&lt;" //hakcipta: www.tips-berbagiifo.blogspot.com
+if (match=="<")
+return "&lt;"
 else if (match==">")
 return "&gt;"
-} //hakcipta: www.tips-berbagiifo.blogspot.com
+}
 //]]>
